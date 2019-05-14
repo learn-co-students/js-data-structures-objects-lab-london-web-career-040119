450a1239f7a640954bb4d56d885f80f05569e24e
0118ayush <ayush_gehlot@hotmail.com>
2019-05-14 21:44:40 +0100

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,30 @@
 // Write your solution in this file!
+
+const driver = {
+    mustang: "Vin Diesel", 
+    hummer: "Dwayne Johnson", 
+    porshe: "Paul Walker", 
+    renault: "Tyrese Gibson"
+}
+
+function updateDriverWithKeyAndValue(driver, key, value){
+    const object = Object.assign({}, driver, {[key]:value})
+    return object
+}
+
+function destructivelyUpdateDriverWithKeyAndValue(driver, key, value){
+   const newObj = Object.assign(driver,{[key]:value})
+    return newObj
+}
+
+function deleteFromDriverByKey(driver, key){
+    const newObj = Object.assign({}, driver)
+    delete newObj[key]
+    return newObj
+
+}
+
+function destructivelyDeleteFromDriverByKey(driver, key){
+    delete driver[key]
+    return driver
+}
